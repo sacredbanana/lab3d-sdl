2589f6d6dca70467fde7a189b0e72b95f5242b19
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2019-07-14 18:00:34 +0800

    Project cleanup

---
+++ macOS/CMakeFiles/Kens-Labyrinth.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/cameron/Downloads/lab3d-sdl/src/adlibemu.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/adlibemu.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/demo.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/demo.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/graphx.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/graphx.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/init.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/init.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/lab3d.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/lab3d.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/launcher.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/launcher.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/oldlab3d.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/oldlab3d.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/setup.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/setup.c.o"
+  "/Users/cameron/Downloads/lab3d-sdl/src/subs.c" "/Users/cameron/Downloads/lab3d-sdl/macOS/CMakeFiles/Kens-Labyrinth.dir/src/subs.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "KEN_64BIT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../external/SDL2/include"
+  "../external/SDL2_image/include"
+  "../external/zlib/include"
+  "/opt/X11/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
