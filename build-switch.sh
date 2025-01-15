ddacd13cae55490f60537423184e876b1e7b2909
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2025-01-15 22:31:24 +0800

    Move Switch package step from build script to makefile

--- build-switch.sh
+++ build-switch.sh
@@ -7,17 +7,4 @@ docker run --rm \
     -v ${PWD}:/work \
     -e USER=$( id -u ) -e GROUP=$( id -g ) \
     -w /work \
-    -it devkitpro/devkita64 /bin/bash -c "make clean -f Makefile.Switch && make -f Makefile.Switch"
-
-mkdir -p dist/ken
-rm -dr dist/switch || true
-cp dist/Kens-Labyrinth.nro dist/ken/
-cp -r gamedata dist/ken/
-cd dist
-zip -r ken/ken.zip ken/
-mv ken switch
-rm -f Kens-Labyrinth.nro
-rm -f Kens-Labyrinth.nacp
-rm -f Kens-Labyrinth.nso
-rm -f Kens-Labyrinth.pfs0
-rm -f Kens-Labyrinth.elf
+    -it devkitpro/devkita64 /bin/bash -c "make clean package -f Makefile.Switch"
